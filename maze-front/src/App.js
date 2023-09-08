fe328f8a79363a45a4bc0ece1b6fd98cd7ca6afc
r20barou <romain.barou@imt-atlantique.net>
2023-09-08 16:17:24 +0200

    ajout maze

---
+++ maze-front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import MainPage from './components/templates/mainPage';
+
+function App() {
+  return (
+  <>
+    <MainPage></MainPage>
+  </>
+  );
+}
+
+export default App;
---
+++ maze-front/src/components/templates/mainPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1>Hi, I'm the main page!</h1>
+    </div>
+  );
+}
